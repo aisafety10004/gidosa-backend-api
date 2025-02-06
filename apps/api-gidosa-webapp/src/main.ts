0560df82db5ae5e14219ad003f0b1e46cafaed7c
pktjesus1 <pktjesus@gmail.com>
2025-02-07 00:09:13 +0900

    urlconst오류 수정1

--- apps/api-gidosa-webapp/src/main.ts
+++ apps/api-gidosa-webapp/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGidosaWebappModule } from './api-gidosa-webapp.module';
+// import { UrlConst } from '@app/gidosa-common-api/constants/gidosa/UrlConst';
+import { UrlConst } from '../../../libs/gidosa-common-api/constants/gidosa/UrlConst';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGidosaWebappModule);
+
+  // cors 설정
+  app.enableCors({
+    origin: [process.env.ORIGIN],
+    credentials: true,
+  });
+  app.setGlobalPrefix(UrlConst.PREFIX_API_BASE);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
